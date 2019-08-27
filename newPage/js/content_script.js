35fc8c9644749b236667acd763fe3015d62998bc
张扬 <zhangyang@dangdang.com>
2019-08-27 17:06:22 +0800

    新建拓展-谷歌搜索打开新标签页

---
+++ newPage/js/content_script.js
@@ -0,0 +1,12 @@
+//注入页面的脚本文件
+(function () {
+    $(function () {
+        const $searchContent = $("h3");
+        const $len = $searchContent.length;
+        if ($len <= 0) return;
+
+        for (let i = 0; i < $len; i++) {
+            $searchContent[i].parentElement.nodeName === "A" && $searchContent[i].parentElement.setAttribute('target', '_blank');
+        }
+    })
+})()
